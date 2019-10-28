33fbb0f038ef7866a4c5f8b40ed466e65be44a0c
Diego Gomez <diegogomezchivas@gmail.com>
2019-10-28 01:04:42 -0600

    vikingos

--- module-1/lab-data-vikings/vikingsClases.py
+++ module-1/lab-data-vikings/vikingsClases.py
@@ -1,21 +1,60 @@
 
 # Soldier
 
-
 class Soldier:
-    pass
+    def __init__(self, health, strength):
+        self.health = health
+        self.strength = strength
+    def attack(self):
+        return self.strength
+    def receiveDamage(self, damage):
+        self.health = self.health - int(damage)
+
+
 
 # Viking
 
 
-class Viking:
+class Viking(Soldier):
+    def __init__(self, name, health, strength):
+        Soldier.__init__(self, health,strength)
+        self.name = name
+        self.health=health
+        self.strength=strength
+    def attack(self):
+        return Soldier.attack(self)
+    def receiveDamage(self, dam):
+        puntosrecibidos = dam
+        self.health = self.health - int(puntosrecibidos)
+        if self.health > 0:
+            print(self.name + ' has receive {} points of damage'.format(puntosrecibidos))
+        else:
+            print(self.name + ' has died in combat')
+    def battleCry(self):
+        return "Odin Owns You All!"
     pass
+
+
+
+
 
 # Saxon
 
 
-class Saxon:
+class Saxon(Soldier):
+    def __init__(self, health, strength):
+        Soldier.__init__(self, health,strength)
+        self.health=health
+        self.strength=strength
+    def receiveDamage(self, dam):
+        puntosrecibidos = dam
+        self.health = self.health - int(puntosrecibidos)
+        if self.health > 0:
+            print('Saxon has receive {} points of damage'.format(puntosrecibidos))
+        else:
+            print('Saxon has died in combat')
     pass
+
 
 # War
 
